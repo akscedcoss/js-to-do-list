653c4c59b323562c17a1ff19dd557ba0c82d8dd1
cedcoss <amnakumarsrivastava@cedcoss.com>
2022-02-17 19:13:39 +0530

    check uncheck implemented

--- src/myjs.js
+++ src/myjs.js
@@ -6,6 +6,7 @@ function create_task () {
   task = document.getElementById('new-task').value
   task_arr.push({ id: id_logic++, type: 'unchecked', task: task })
   display_unchecked()
+  display_checked()
 }
 // display unchecked function
 function display_unchecked () {
@@ -13,7 +14,32 @@ function display_unchecked () {
   div.innerHTML = ''
   task_arr.forEach(element => {
     if (element['type'] == 'unchecked') {
-      div.innerHTML += `<li><input type="checkbox"><label>${element['task']}</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>`
+      div.innerHTML += `<li><input type="checkbox" onchange="check_unchek (${element['id']})"><label>${element['task']}</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>`
     }
   })
+}
+// display uncheck function
+function display_checked () {
+  div = document.getElementById('completed-tasks')
+  task_arr.forEach(element => {
+    if (element['type'] == 'checked') {
+      div.innerHTML += `<li><input type="checkbox" onchange="check_unchek (${element['id']})" checked><label>${element['task']}</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>`
+    }
+  })
+}
+
+// change status
+function check_unchek (id) {
+  console.log(id)
+  task_arr.forEach(element => {
+    if (id == element['id']) {
+      if (element['type'] == 'unchecked') {
+        element['type'] = 'checked'
+      } else {
+        element['type'] = 'unchecked'
+      }
+    }
+  })
+  display_unchecked()
+  display_checked()
 }
